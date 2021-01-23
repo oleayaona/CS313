8a123c8fc61fdc22ac89e91e1d00b7ba6f649292
Olea <oleayaona@gmail.com>
2021-01-24 06:39:26 +0800

    links

--- web/week_03/shopping_cart/view/homepage.php
+++ web/week_03/shopping_cart/view/homepage.php
@@ -20,7 +20,7 @@
             <p>Lifestyle and Home Decor</p>
         </div>
         <nav>
-            <?php require $_SERVER['DOCUMENT_ROOT'] . './modules/nav.php'; ?>
+            <?php include $_SERVER['DOCUMENT_ROOT'] . 'web/week_03/shopping_cart/modules/nav.php'; ?>
         </nav>
     </header>
 
