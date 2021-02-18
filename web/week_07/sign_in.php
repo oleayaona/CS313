97194aa8811002c89b9e86245a2230d6526dffc5
Olea <oleayaona@gmail.com>
2021-02-18 08:40:20 +0800

    team

--- web/week_07/sign_in.php
+++ web/week_07/sign_in.php
@@ -23,6 +23,37 @@ function registerUser($username, $password) {
     $stmt->closeCursor();
 }
 
+function dbConnect(){
+    
+    try {
+		// default Heroku Postgres configuration URL
+		$dbUrl = getenv('DATABASE_URL');
+
+		// Get the various parts of the DB Connection from the URL
+		$dbopts = parse_url($dbUrl);
+
+		$dbHost = $dbopts["host"];
+		$dbPort = $dbopts["port"];
+		$dbUser = $dbopts["user"];
+		$dbPassword = $dbopts["pass"];
+		$dbName = ltrim($dbopts["path"],'/');
+
+		// Create the PDO connection
+		$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+		// this line makes PDO give us an exception when there are problems, and can be very helpful in debugging!
+		$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+
+		return $db;
+	}
+	catch (PDOException $e) {
+		echo "Error connecting to DB. Details: $e";
+		// header('Location: /web/shop/view/500.php');
+		exit;
+	}
+
+}
+
 ?><!DOCTYPE html>
 <html lang="en">
 
